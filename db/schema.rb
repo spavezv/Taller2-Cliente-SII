12efcb0cc075fd53e01a4cc911a785317c450d9e
Francisco Moya <zulkic@gmail.com>
2014-11-02 16:52:20 -0300

    Agregada la migración de usuarios
    La había borrado :P

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141101220705) do
+ActiveRecord::Schema.define(version: 20141102165122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,14 +48,14 @@ ActiveRecord::Schema.define(version: 20141101220705) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 4,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.integer  "tipo_de_usuario",        default: 4,  null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "tipo_de_usuario",        default: 4,  null: false
   end
 
   add_index "usuarios", ["email"], name: "index_usuarios_on_email", unique: true, using: :btree
