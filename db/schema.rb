8955889aa00dcc9971658346a512f37f1351806b
fraflores <fraflores.cl@gmail.com>
2014-11-13 02:54:29 -0300

    Corrección de calendarios en nueva tarea

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20141113032102) do
-=======
-ActiveRecord::Schema.define(version: 20141113031806) do
->>>>>>> origin/master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,7 +49,6 @@ ActiveRecord::Schema.define(version: 20141113031806) do
     t.date     "termino"
     t.text     "descripcion"
     t.integer  "servicio_id"
-    t.integer  "ejecutante"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "usuario_id"
