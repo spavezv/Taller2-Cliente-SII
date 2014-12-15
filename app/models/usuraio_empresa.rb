867692187a4146528dce16655035885fca21b983
josemar <lestat.baeza@gmail.com>
2014-12-14 21:22:10 -0300

    Eliminación  de tabla usuario_empresas
    Se elimino la tabla que no se utiliza y se ajustaron las empresas
    creadas con el archivo seeds (mayúsculas y agregación de más
    información)

--- app/models/usuraio_empresa.rb
+++
@@ -1,2 +0,0 @@
-class UsuraioEmpresa < ActiveRecord::Base
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,37 +6,38 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-#creando administradores
-Usuario.create(email: 'admin@mimemo.cl', password: '12345678', tipo_de_usuario: 2, nombre: 'admin')
-Usuario.create(email: 'admin2@mimemo.cl', password: '12345678', tipo_de_usuario: 2, nombre: 'admin2')
-Usuario.create(email: 'admin3@mimemo.cl', password: '12345678', tipo_de_usuario: 2, nombre: 'admin3')
 
-#creando solo un administrador
-Usuario.create(email: 'superadmin@mimemo.cl', password: '12345678', tipo_de_usuario: 1, nombre: 'superadmin')
+#creando solo un super dministrador
+Usuario.create(email: 'superadmin@mimemo.cl', password: '12345678', tipo_de_usuario: 1, nombre: 'Super Administrador')
+
+#creando administradores
+Usuario.create(email: 'admin1@mimemo.cl', password: '12345678', tipo_de_usuario: 2, nombre: 'Administrador 1')
+Usuario.create(email: 'admin2@mimemo.cl', password: '12345678', tipo_de_usuario: 2, nombre: 'Administrador 2')
+Usuario.create(email: 'admin3@mimemo.cl', password: '12345678', tipo_de_usuario: 2, nombre: 'Administrador 3')
 
 #creando ejecutantes pero quedan como clientes
-Usuario.create(email: 'ejecutante@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'ejecutante')
+Usuario.create(email: 'ejecutante1@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'ejecutante')
 Usuario.create(email: 'ejecutante2@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'ejecutante2')
 Usuario.create(email: 'ejecutante3@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'ejecutante3')
 Usuario.create(email: 'ejecutante4@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'ejecutante4')
 
 #creando clientes
-Usuario.create(email: 'cliente@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'cliente')
-Usuario.create(email: 'cliente2@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'cliente2')
-Usuario.create(email: 'cliente3@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'cliente3')
-Usuario.create(email: 'cliente4@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'cliente4')
-Usuario.create(email: 'cliente5@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'cliente5')
-Usuario.create(email: 'cliente6@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'cliente6')
-Usuario.create(email: 'cliente7@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'cliente7')
-Usuario.create(email: 'cliente8@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'cliente8')
+Usuario.create(email: 'cliente1@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'Cliente 1')
+Usuario.create(email: 'cliente2@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'Cliente 2')
+Usuario.create(email: 'cliente3@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'Cliente 3')
+Usuario.create(email: 'cliente4@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'Cliente 4')
+Usuario.create(email: 'cliente5@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'Cliente 5')
+Usuario.create(email: 'cliente6@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'Cliente 6')
+Usuario.create(email: 'cliente7@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'Cliente 7')
+Usuario.create(email: 'cliente8@mimemo.cl', password: '12345678', tipo_de_usuario: 4, nombre: 'Cliente 8')
 
 #creando algunas empresas
-Empresa.create(nombre_fantasia: 'falabella', nombre_comercial: 'falabella', correo: 'falabella@mimemo.cl', direccion: 'curico')
-Empresa.create(nombre_fantasia: 'ripley', nombre_comercial: 'ripley', correo: 'ripley@mimemo.cl', direccion: 'curico')
-Empresa.create(nombre_fantasia: 'andesgear', nombre_comercial: 'andesgear', correo: 'andesgear@mimemo.cl', direccion: 'curico')
-Empresa.create(nombre_fantasia: 'paris', nombre_comercial: 'paris', correo: 'paris@mimemo.cl', direccion: 'curico')
-Empresa.create(nombre_fantasia: 'bancochile', nombre_comercial: 'bancochile', correo: 'bancochile@mimemo.cl', direccion: 'curico')
-Empresa.create(nombre_fantasia: 'hospitalcurico', nombre_comercial: 'hospitalcurico', correo: 'hospitalcurico@mimemo.cl', direccion: 'curico')
-Empresa.create(nombre_fantasia: 'soler', nombre_comercial: 'soler', correo: 'soler@mimemo.cl', direccion: 'curico')
-Empresa.create(nombre_fantasia: 'doggis', nombre_comercial: 'doggis', correo: 'doggis@mimemo.cl', direccion: 'curico')
-Empresa.create(nombre_fantasia: 'juanmaestro', nombre_comercial: 'juanmaestro', correo: 'juanmaestro@mimemo.cl', direccion: 'curico')
+Empresa.create(nombre_fantasia: 'Falabella', nombre_comercial: 'Falabella', correo: 'falabella@mimemo.cl', web: 'www.falabella.com', telefono: '075-226587', direccion: 'Curicó')
+Empresa.create(nombre_fantasia: 'Ripley', nombre_comercial: 'Ripley', correo: 'ripley@mimemo.cl', web: 'www.ripley.com', telefono: '075-226587', direccion: 'Curicó')
+Empresa.create(nombre_fantasia: 'Andesgear', nombre_comercial: 'Andesgear', correo: 'andesgear@mimemo.cl', web: 'www.andesgear.com', telefono: '075-226587', direccion: 'Curicó')
+Empresa.create(nombre_fantasia: 'Paris', nombre_comercial: 'Paris', correo: 'paris@mimemo.cl', web: 'www.paris.com', telefono: '075-226587', direccion: 'Curicó')
+Empresa.create(nombre_fantasia: 'Banco de Chile', nombre_comercial: 'Banco de Chile', correo: 'bancochile@mimemo.cl', web: 'www.bancochile.com', telefono: '075-226587', direccion: 'Curicó')
+Empresa.create(nombre_fantasia: 'Hospital de Curicó', nombre_comercial: 'Hospital de Curico', correo: 'hospitalcurico@mimemo.cl', web: 'www.hospitalcurico.com', telefono: '075-226587', direccion: 'Curicó')
+Empresa.create(nombre_fantasia: 'Soler', nombre_comercial: 'Soler', correo: 'soler@mimemo.cl', web: 'www.soler.com', telefono: '075-226587', direccion: 'Curicó')
+Empresa.create(nombre_fantasia: 'Doggis', nombre_comercial: 'Doggis', correo: 'doggis@mimemo.cl', web: 'www.doggis.com', telefono: '075-226587', direccion: 'Curicó')
+Empresa.create(nombre_fantasia: 'Juan Maestro', nombre_comercial: 'Juan Maestro', correo: 'juanmaestro@mimemo.cl', web: 'www.juanmaestro.com', telefono: '075-226587', direccion: 'Curicó')
